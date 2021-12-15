b0eb6efa9d7754a4915300814ad05c6d5e62a9c4
Lucas Luize <lucasluize.tech@gmail.com>
2021-12-15 17:00:46 -0500

    added some styling to dogos

--- src/DogDetails.js
+++ src/DogDetails.js
@@ -18,22 +18,31 @@ const DogDetails = ({ dogs }) => {
     history.push("/dogs");
   };
 
+  const handleAdopt = () => {
+    alert(`You have Adopted ${dog[0].name}`);
+  };
+
   return (
-    <MDBCard style={{ maxWidth: "22rem" }}>
-      <MDBCardImage src={dog[0].src} position='top' alt={dog[0].name} />
-      <MDBCardBody>
-        <MDBCardTitle>{dog[0].name}</MDBCardTitle>
-        <MDBCardText>
-          <h5>{dog[0].age} yrs.</h5>
-          <ul>
-            {dog[0].facts.map((fact, idx) => (
-              <li key={idx}>{fact}</li>
-            ))}
-          </ul>
-        </MDBCardText>
-        <MDBBtn onClick={handleClick}>Go Back</MDBBtn>
-      </MDBCardBody>
-    </MDBCard>
+    <div className='DogDetails container'>
+      <MDBCard style={{ maxWidth: "22rem" }}>
+        <MDBCardImage src={dog[0].src} position='top' alt={dog[0].name} />
+        <MDBCardBody>
+          <MDBCardTitle>{dog[0].name}</MDBCardTitle>
+          <MDBCardText>
+            <h5>{dog[0].age} yrs.</h5>
+            <ul>
+              {dog[0].facts.map((fact, idx) => (
+                <li key={idx}>{fact}</li>
+              ))}
+            </ul>
+          </MDBCardText>
+          <MDBBtn onClick={handleClick} className='mx-2'>
+            Go Back
+          </MDBBtn>
+          <MDBBtn onClick={handleAdopt}>Adopt</MDBBtn>
+        </MDBCardBody>
+      </MDBCard>
+    </div>
   );
 };
 
